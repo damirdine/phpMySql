285244be4ce6a1d6b535022f3df740b2aa246bb9
damirdine <damirdine@outlook.fr>
2022-05-17 09:21:25 +0200

    maj

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>myPhpApp</title>
-</head>
-<body>
-    
-    <?php 
-        $name = 'damirdine';
-        echo('hello '.$name.' the boss')
-        ?>
-    <a href="http://localhost/phpMySql/contact.php">go to contact page</a>
-
-</body>
-</html>
+<?php include_once('home.php') ?>
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php include_once('header.php')?>
+
+<form action="submit_contact.php" method="POST">
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+        <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe</label>
+        <textarea class="form-control" placeholder="Votre mot de passe" id="password" name="password"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Me connecter</button>
+</form>
+
+<?php include_once('footer.php') ?>
