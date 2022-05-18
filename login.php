ab2c6b2547dd1c1fd2649e0dd88a4ff241f8b3f4
damirdine <damirdine@outlook.fr>
2022-05-18 20:29:22 +0200

    succes and error messge gestion

--- login.php
+++ login.php
@@ -20,21 +20,22 @@ if (isset($postMail)  && isset($postPassword)) {
     }
 }
 ?>
-<?php if(!isset($_SESSION['logged_user'])):?>
-    <form class='container' action='./home.php' method="POST">
-        <h1>Connexion</h1>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
-            <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Mot de passe</label>
-            <input type='password' class="form-control" placeholder="Votre mot de passe" id="password" name="password"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Me connecter</button>
-    </form>
-
-<?php else: ?>
-        <p class="alert alert-success">Bonjour <?php echo($_SESSION['logged_user_name'])?> et bienvenue sur le site !</p>
+<?php if(isset($_SESSION['logged_user'])):?>   
+    <p class="alert alert-success">Bonjour <?php echo($_SESSION['logged_user_name'])?> et bienvenue sur le site !</p>
+<?php elseif($errorMessage):?>
+    <p class="alert alert-danger"> <?php echo($errorMessage)?></p>
 <?php endif; ?>
+    
+<form class='container' action='./home.php' method="POST">
+    <h1>Connexion</h1>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+        <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type='password' class="form-control" placeholder="Votre mot de passe" id="password" name="password"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Me connecter</button>
+</form>
