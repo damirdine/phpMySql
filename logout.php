bf57a0be934da35da142bd71b49af777297e0c2a
damirdine <damirdine@outlook.fr>
2022-05-18 20:43:46 +0200

    succes and error messge gestion

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@
     include_once('variables.php');
     include_once('header.php');
 ?>
-<p class="alert alert-success">Vous ete deconnecter</p>
+<p class="alert alert-warning">Vous ete deconnecter</p>
 <a class="btn btn-primary" href="./home.php"> retouerner a l'acceuil</a>
 
 <?php include_once('footer.php'); ?>
