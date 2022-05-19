94ca7c9137d08f4d9f505ae699fed7fa11fdfa9d
damirdine <damirdine@outlook.fr>
2022-05-19 10:57:02 +0200

    add 'htmlspecialchars' for prevent XXS failure

--- login.php
+++ login.php
@@ -15,14 +15,14 @@ if (isset($postMail)  && isset($postPassword)) {
             $_SESSION['logged_user_name'] = $user['full_name'];
 
         } else {
-            $errorMessage = sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)', $_POST['email'], $_POST['password']);
+            $errorMessage = htmlspecialchars(sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)', $_POST['email'], $_POST['password'])) ;
         }
     }
 }
 ?>
 <?php if(isset($_SESSION['logged_user'])):?>   
 
-    <p class="alert alert-success">Bonjour <?php echo($_SESSION['logged_user_name'])?> et bienvenue sur le site !</p>
+    <p class="alert alert-success">Bonjour <?php htmlspecialchars($_SESSION['logged_user_name'])?> et bienvenue sur le site !</p>
     
 <?php elseif($errorMessage):?>
 
@@ -31,12 +31,12 @@ if (isset($postMail)  && isset($postPassword)) {
     <h1>Connexion</h1>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" required>
         <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
     </div>
     <div class="mb-3">
         <label for="password" class="form-label">Mot de passe</label>
-        <input type='password' class="form-control" placeholder="Votre mot de passe" id="password" name="password"></textarea>
+        <input type='password' class="form-control" placeholder="Votre mot de passe" id="password" name="password" required>
     </div>
     <button type="submit" class="btn btn-primary">Me connecter</button>
 </form>
@@ -46,12 +46,12 @@ if (isset($postMail)  && isset($postPassword)) {
     <h1>Connexion</h1>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" required>
         <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
     </div>
     <div class="mb-3">
         <label for="password" class="form-label">Mot de passe</label>
-        <input type='password' class="form-control" placeholder="Votre mot de passe" id="password" name="password"></textarea>
+        <input type='password' class="form-control" placeholder="Votre mot de passe" id="password" name="password" required>
     </div>
     <button type="submit" class="btn btn-primary">Me connecter</button>
 </form>
