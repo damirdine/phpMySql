38e571cc14c87102e11f284747b762e9982bdb55
damirdine <damirdine@outlook.fr>
2022-05-23 14:34:44 +0200

    View PDO error details

--- pdo.php
+++ pdo.php
@@ -3,7 +3,8 @@ try{
     $db = new PDO(
         'mysql:host=localhost;dbname=site_recettes;charset=utf8',
         'root',
-        ''
+        '',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
     );
 }catch(Exception $e){
     die('Erreur : '.$e->getMessage());
