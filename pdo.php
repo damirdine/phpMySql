8d898cff6ef05c55b767cae1cf9591ee6c88b823
damirdine <damirdine@outlook.fr>
2022-05-23 13:54:51 +0200

    sort with sql request

--- pdo.php
+++ pdo.php
@@ -9,13 +9,13 @@ try{
     die('Erreur : '.$e->getMessage());
 }
 
-$sqlQuery= 'SELECT * FROM recettes';
+$sqlQuery= 'SELECT * FROM recettes WHERE is_enabled = TRUE';
 $recipesStatement = $db->prepare($sqlQuery);
 $recipesStatement->execute();
 $recipes = $recipesStatement->fetchAll();
 
 foreach($recipes as $recipe) {
-    if($recipe['is_enabled']==1){
+    //if($recipe['is_enabled']==1){
 ?>
 
     <p><?php echo $recipe['title']?></p>
@@ -23,5 +23,5 @@ foreach($recipes as $recipe) {
     <p><?php echo $recipe['recipe']?></p>
     <hr>
 
-<?php }
+<?php //}
 } ?>
