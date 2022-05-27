935a94047debed0ff52f3fc945ca1b8d519cafce
damirdine <damirdine@outlook.fr>
2022-05-27 18:38:00 +0200

    recipe page with comment form

--- index.php
+++ index.php
@@ -15,22 +15,22 @@
 
 <?php if ($_SESSION['logged_user']) : ?>
     <?php foreach ($recipes as $recipe) : ?>
-        <article>
-            <h3><?php echo ($recipe['title']); ?></h3>
+        <article class="my-4">
+            <a class="text-dark" href="./post.php?recipe_id=<?php echo($recipe['recpe_id'])?>"><h3><?php echo ($recipe['title']); ?></h3></a>
             <div><?php echo ($recipe['recipe']); ?></div>
             <i><?php echo ($recipe['author']); ?></i>
+            <?php if ($_SESSION['logged_user']==$recipe['author']) : ?>
+                <div class="btn-container d-flex gap-3">
+                    <form action="./edit_recipe.php" methode="GET">
+                        <input type="hidden" name="recipe_id" value="<?php echo ($recipe['recpe_id']); ?>">
+                        <input type="submit" class="btn btn-warning" value="Editer">
+                    </form>
+                    <form action="./delete_recipe.php" methode="GET">
+                        <input type="hidden" name="recipe_id" value="<?php echo ($recipe['recpe_id']); ?>">
+                        <input type="submit" class="btn btn-danger" value="Supprimer">
+                    </form>
+                </div>
         </article>
-        <?php if ($_SESSION['logged_user']==$recipe['author']) : ?>
-            <div class="btn-container d-flex gap-3">
-                <form action="./edit_recipe.php" methode="GET">
-                    <input type="hidden" name="recipe_id" value="<?php echo ($recipe['recpe_id']); ?>">
-                    <input type="submit" class="btn btn-warning" value="Editer">
-                </form>
-                <form action="./delete_recipe.php" methode="GET">
-                    <input type="hidden" name="recipe_id" value="<?php echo ($recipe['recpe_id']); ?>">
-                    <input type="submit" class="btn btn-danger" value="Supprimer">
-                </form>
-            </div>
         <?php endif;?>
     <?php endforeach ?>
 <?php endif; ?>
---
+++ post.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include 'variables.php';
+include_once 'header.php';
+if(isset($_GET['recipe_id'])){
+    $sqlRecipeQuery= 'SELECT * FROM recettes WHERE recpe_id = :recpe_id' ;
+    $recipeStatement = $db->prepare($sqlRecipeQuery);
+    $recipeStatement->execute(
+        ['recpe_id' => $_GET['recipe_id'],]
+    )or die(print_r($db->errorInfo()));
+    $recipe = $recipeStatement->fetch();
+}
+?>
+<?php if(isset($recipe)):?>
+    <?php if(isset($_SESSION['logged_user'])):?>
+        <h1 class="my-5"><?php echo($recipe['title']) ?></h1>
+        <p><?php echo($recipe['recipe'])?></p>
+        <hr>
+        <form class="mt-4" action="./post" method="POST">
+            <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Commentaire</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" name='update_recipe' placeholder="Détailler votre recette" rows="2" required></textarea>
+            </div>
+            <input type="hidden" name="recipe_id" value="<?php echo($recipe['recpe_id']);?>">
+            <button type="submit" class="btn btn-warning mb-3">commenter</button>
+        </div>
+        </form>
+    <?php else:?>
+        <p class="alert alert-warning mt-5">Vous ne pouvez pas editer des recettes qui ne sont pas les votre</p>
+<?php endif;
+endif;
+include_once 'footer.php';
+?>
