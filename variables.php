e510f227ccd93acac5739e6294cbe3d3a19f1d12
damirdine <damirdine@outlook.fr>
2022-05-30 09:29:20 +0200

    db local

--- variables.php
+++ variables.php
@@ -8,17 +8,17 @@ if(isset($_GET['limit']) && is_numeric($_GET['limit'])) {
 
 ///// DB CONFIG
 
-//$DB_HOST = 'localhost';
-//$DB_NAME = 'site_recettes';
-//$DB_USER = 'root';
-//$DB_PASSWORD = '';
+$DB_HOST = 'localhost';
+$DB_NAME = 'site_recettes';
+$DB_USER = 'root';
+$DB_PASSWORD = '';
 
 //PROD DB
 
-$DB_HOST = 'eu-cdbr-west-02.cleardb.net'; 
-$DB_NAME = 'heroku_b78654bfa6ae6d8';
-$DB_USER = 'bcfcecbbcd0988'; 
-$DB_PASSWORD = 'cfa88139';
+//$DB_HOST = 'eu-cdbr-west-02.cleardb.net'; 
+//$DB_NAME = 'heroku_b78654bfa6ae6d8';
+//$DB_USER = 'bcfcecbbcd0988'; 
+//$DB_PASSWORD = 'cfa88139';
 
 $db = new PDO(
     "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8",
